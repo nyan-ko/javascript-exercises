bd977c07a294c2ebc1c1d413f314e6086d7c8680
nyan-ko <dumbstupidcatboy@gmail.com>
2022-07-30 13:58:08 -0400

    Complete exercises 1-7

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let arr = string.split("");
+    let reverseArr = new Array();
+    for (let i = 0; i < arr.length; i++) {
+        reverseArr[i] = arr[arr.length - i - 1];
+    }
+    return reverseArr.join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
 const removeFromArray = function() {
-
+    let shift = 0;
+    let arr = arguments[0];
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 1; j < arguments.length; j++) {
+            if (arr[i] === arguments[j]) {
+                arr[i] = undefined;
+                shift++;
+                break;
+            }
+        }
+        if (arr[i] && shift > 0) {
+            arr[i - shift] = arr[i];
+            arr[i] = undefined;
+        }
+    }
+    return arr.slice(0, arr.length - shift);
 };
 
 // Do not edit below this line
